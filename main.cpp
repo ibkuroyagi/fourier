3cd03fea5a12866fd61c061ad8a7cc880381a870
ibukikuroyanagi <ibkuroyagi@gmail.com>
2019-10-21 12:32:09 +0900

    高速フーリエ変換

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 #include <math.h>
 #pragma warning(disable : 4996)
 #include "ImageIO.h"
-
-#define pi 3.141592653
+const double PI = 3.141592653589793;
 #define SIZE 4096
 
 //グレースケール画像の二値化を行う
@@ -30,7 +29,74 @@ void Binarization(Image<GRAY> src, Image<GRAY> &dst, int threshold)
         }
 }
 
-// one_dimension_fourier
+
+// 高速フーリエ変換
+// int n ：データ数（２のべき乗）
+// int flg ：順変換:-1, 逆変換:1
+// double *ar ：データ配列（実部；要素数 n)
+// double *ai ：データ配列（虚部；要素数 n）
+int fft(int n, int flg, double *ar, double *ai)
+{
+  long m, mh, i, j, k, irev;
+  double wr, wi, xr, xi;
+  double theta;
+
+  theta = flg * 2 * PI / n;
+
+  i = 0;
+  for (j = 1; j < n - 1; j++)
+  {
+    for (k = n >> 1; k > (i ^= k); k >>= 1)
+      ;
+    if (j < i)
+    {
+      xr = ar[j];
+      xi = ai[j];
+      ar[j] = ar[i];
+      ai[j] = ai[i];
+      ar[i] = xr;
+      ai[i] = xi;
+    }
+  }
+  for (mh = 1; (m = mh << 1) <= n; mh = m)
+  {
+    irev = 0;
+    for (i = 0; i < n; i += m)
+    {
+      wr = cos(theta * irev);
+      wi = sin(theta * irev);
+      for (k = n >> 2; k > (irev ^= k); k >>= 1)
+        ;
+      for (j = i; j < mh + i; j++)
+      {
+        k = j + mh;
+        xr = ar[j] - ar[k];
+        xi = ai[j] - ai[k];
+        ar[j] += ar[k];
+        ai[j] += ai[k];
+        ar[k] = wr * xr - wi * xi;
+        ai[k] = wr * xi + wi * xr;
+      }
+    }
+  }
+
+  if (flg == -1)
+  {
+    for (i = 0; i<n; i++)
+    {
+      ar[i] /= n;
+      ai[i] /= n;
+    }
+  }
+  return 0;
+}
+
+// フーリエ変換
+// int N ：データ数
+// double *re ：入力データ配列（実部；要素数 N)
+// double *im ：入力データ配列（虚部；要素数 N）
+// double *Re ：出力データ配列（実部；要素数 N)
+// double *Im ：出力データ配列（虚部；要素数 N）
 double dft(double re[], double im[], double Re[], double Im[], int N)
 {
         //実数部分と虚数部分に分けてフーリエ変換
@@ -38,13 +104,19 @@ double dft(double re[], double im[], double Re[], double Im[], int N)
         {
                 for (int n = 0; n < N; n++)
                 {
-                        Re[k] += (re[n] * cos(2 * pi * k * n / N) + im[n] * sin(2 * pi * k * n / N));
-                        Im[k] += (-1 * re[n] * sin(2 * pi * k * n / N) + im[n] * cos(2 * pi * k * n / N));
+                        Re[k] += (re[n] * cos(2 * PI * k * n / N) + im[n] * sin(2 * PI * k * n / N));
+                        Im[k] += (-1 * re[n] * sin(2 * PI * k * n / N) + im[n] * cos(2 * PI * k * n / N));
+                        // Im[k] = 0.0;
                 }
         }
 }
 
-// invert_one_dimension_fourier
+// 逆フーリエ変換
+// int N ：データ数
+// double *Re ：入力データ配列（実部；要素数 N)
+// double *Im ：入力データ配列（虚部；要素数 N）
+// double *re ：出力データ配列（実部；要素数 N)
+// double *im ：出力データ配列（虚部；要素数 N）
 double idft(double Re[], double Im[], double re[], double im[], int N)
 {
         //実数部分と虚数部分に分けて逆フーリエ変換
@@ -52,19 +124,20 @@ double idft(double Re[], double Im[], double re[], double im[], int N)
         {
                 for (int n = 0; n < N; n++)
                 {
-                        re[k] += (Re[n] * cos(2 * pi * k * n / N) - Im[n] * sin(2 * pi * k * n / N));
-                        im[k] += (Re[n] * sin(2 * pi * k * n / N) + Im[n] * cos(2 * pi * k * n / N));
+                        re[k] += (Re[n] * cos(2 * PI * k * n / N) - Im[n] * sin(2 * PI * k * n / N));
+                        im[k] += (Re[n] * sin(2 * PI * k * n / N) + Im[n] * cos(2 * PI * k * n / N));
+                        // im[k] = 0.0;
                 }
                 re[k] /= N;
                 im[k] /= N;
         }
 }
 
-void two_dimension_fourier(Image<GRAY> src, Image<GRAY> &dst)
+void two_dimension_fourier(Image<GRAY> src, Image<GRAY> &dst, char function[])
 {
         double re_arr[SIZE], im_arr[SIZE], Re_tmp[SIZE], Im_tmp[SIZE];
         double Re_arr[256][256], Im_arr[256][256];      //こいつやばい
-        printf("src.data[0][255] = %d\n",src.data[0][255]);
+        // printf("src.data[0][255] = %d\n",src.data[0][255]);
 
         for (int i = 0; i < src.H; i++)
         {
@@ -73,12 +146,22 @@ void two_dimension_fourier(Image<GRAY> src, Image<GRAY> &dst)
                         re_arr[j] = (double)src.data[i][j];
                         im_arr[j] = 0.0;
                 }
-                dft(re_arr, im_arr, Re_tmp, Im_tmp, src.W);
-                // printf("i = %d, Re_tmp[0] = %lf, Im_tmp[0] = %lf\n",i,Re_tmp[0]);
-                for (int j = 0; j < src.W; j++)
-                {
-                        Re_arr[i][j] = Re_tmp[j];
-                        Im_arr[i][j] = Im_tmp[j];
+                if(strcmp(function,"dft")==0){
+                        dft(re_arr, im_arr, Re_tmp, Im_tmp, src.W);
+                        // printf("i = %d, Re_tmp[0] = %lf\n",i,Re_tmp[0]);
+                        for (int j = 0; j < src.W; j++)
+                        {
+                                Re_arr[i][j] = Re_tmp[j];
+                                Im_arr[i][j] = Im_tmp[j];
+                        }
+                }else if(strcmp(function,"fft")==0){
+                        fft(src.W,-1,re_arr, im_arr);
+                        // printf("i = %d, re_arr[0] = %lf\n",i,re_arr[0]);
+                        for (int j = 0; j < src.W; j++)
+                        {
+                                Re_arr[i][j] = re_arr[j];
+                                Im_arr[i][j] = im_arr[j];
+                        }
                 }
         }
         for (int i = 0; i < src.W; i++)
@@ -88,16 +171,25 @@ void two_dimension_fourier(Image<GRAY> src, Image<GRAY> &dst)
                         re_arr[j] = Re_arr[j][i];
                         im_arr[j] = Im_arr[j][i];
                 }
-                dft(re_arr, im_arr, Re_tmp, Im_tmp, src.H);
-                for (int j = 0; j < src.H; j++)
-                {
-                        dst.data[j][i] = (int)Re_tmp[j];
-                        // dst.data[j][i] = Im_tmp[j];
+                if(strcmp(function,"dft")==0){
+                        dft(re_arr, im_arr, Re_tmp, Im_tmp, src.H);
+                        for (int j = 0; j < src.H; j++)
+                        {
+                                dst.data[j][i] = (int)Re_tmp[j];
+                                // dst.data[j][i] = Im_tmp[j];
+                        }
+                }else if(strcmp(function,"fft")==0){
+                        fft(src.H,-1,re_arr, im_arr);
+                        // printf("i = %d, re_arr[0] = %lf\n",i,re_arr[0]);
+                        for (int j = 0; j < src.W; j++)
+                        {
+                                dst.data[j][i] = (int)re_arr[j];
+                        }
                 }
         }
 }
 
-void inverse_two_dimension_fourier(Image<GRAY> src, Image<GRAY> &dst)
+void inverse_two_dimension_fourier(Image<GRAY> src, Image<GRAY> &dst, char function[])
 {
         double re_arr[SIZE], im_arr[SIZE],  Re_tmp[SIZE], Im_tmp[SIZE];
         double Re_arr[256][256], Im_arr[256][256];
@@ -108,13 +200,23 @@ void inverse_two_dimension_fourier(Image<GRAY> src, Image<GRAY> &dst)
                         re_arr[j] = (double)src.data[j][i];
                         im_arr[j] = 0.0;
                 }
-                idft(re_arr, im_arr, Re_tmp, Im_tmp, src.W);
-                // printf("i = %d, Re_tmp[0] = %lf\n",i,Re_tmp[0]);
-                for (int j = 0; j < src.W; j++)
-                {
-                        Re_arr[j][i] = Re_tmp[j];
-                        Im_arr[j][i] = Im_tmp[j];
-                }
+                if(strcmp(function,"idft")==0){
+                        idft(re_arr, im_arr, Re_tmp, Im_tmp, src.W);
+                        // printf("i = %d, Re_tmp[0] = %lf\n",i,Re_tmp[0]);
+                        for (int j = 0; j < src.W; j++)
+                        {
+                                Re_arr[j][i] = Re_tmp[j];
+                                Im_arr[j][i] = Im_tmp[j];
+                        }
+                }else if(strcmp(function,"ifft")==0){
+                        fft(src.W,-1,re_arr, im_arr);
+                        // printf("i = %d, re_arr[0] = %lf\n",i,re_arr[0]);
+                        for (int j = 0; j < src.W; j++)
+                        {
+                                Re_arr[j][i] = re_arr[j];
+                                Im_arr[j][i] = im_arr[j];
+                        }
+                }        
         }
         for (int i = 0; i < src.W; i++)
         {
@@ -123,12 +225,21 @@ void inverse_two_dimension_fourier(Image<GRAY> src, Image<GRAY> &dst)
                         re_arr[j] = Re_arr[i][j];
                         im_arr[j] = Im_arr[i][j];
                 }
-                idft(re_arr, im_arr, Re_tmp, Im_tmp, src.H);
-                for (int j = 0; j < src.H; j++)
-                {
-                        dst.data[i][j] = (int)Re_tmp[j];
-                        // dst.data[j][i] = Im_tmp[j];
-                }
+                if(strcmp(function,"idft")==0){
+                        idft(re_arr, im_arr, Re_tmp, Im_tmp, src.H);
+                        for (int j = 0; j < src.H; j++)
+                        {
+                                dst.data[i][j] = (int)Re_tmp[j];
+                                // dst.data[j][i] = Im_tmp[j];
+                        }
+                }else if(strcmp(function,"ifft")==0){
+                        fft(src.H,-1,re_arr, im_arr);
+                        // printf("i = %d, re_arr[0] = %lf\n",i,re_arr[0]);
+                        for (int j = 0; j < src.W; j++)
+                        {
+                                dst.data[i][j] = (int)re_arr[j];
+                        }
+                }        
         }
 }
 
@@ -138,7 +249,7 @@ int file_load_and_write(char filename1[], char filename2[],char function[]){
         double Re[SIZE], Im[SIZE]; //変換前の配列
         double re[SIZE], im[SIZE]; //変換後の配列
         int i,k,i1,N = 0;
-        printf("fname1 = %s, fname2 = %s, function = %s\n",filename1,filename2,function);
+        // printf("fname1 = %s, fname2 = %s, function = %s\n",filename1,filename2,function);
         fp1 = fopen(filename1, "r"); // ファイルを開く。失敗するとNULLを返す。
         if (fp1 == NULL)
         {
@@ -153,7 +264,7 @@ int file_load_and_write(char filename1[], char filename2[],char function[]){
                 Im[N] = f2;
                 N++;
         }
-        printf("Re[%d] = %f, Im[%d] = %f\n", N - 1, Re[N - 1], N - 1, Im[N - 1]);
+        // printf("Re[%d] = %f, Im[%d] = %f\n", N - 1, Re[N - 1], N - 1, Im[N - 1]);
         fclose(fp1); // ファイルを閉じる
 
         if ((fp2 = fopen(filename2, "w")) == NULL)
@@ -166,8 +277,20 @@ int file_load_and_write(char filename1[], char filename2[],char function[]){
                 dft(Re, Im, re, im, N);
         }else if(strcmp(function,"idft")==0){
                 idft(Re, Im, re, im, N);
+        }else if(strcmp(function,"fft")==0){
+                fft(N, -1, Re, Im);
+                for(int i=0;i<N;i++){
+                        re[i] = Re[i];
+                        im[i] = Im[i];
+                }
+        }else if(strcmp(function,"ifft")==0){
+                fft(N, 1, Re, Im);
+                for(int i=0;i<N;i++){
+                        re[i] = Re[i];
+                        im[i] = Im[i];
+                }
         }
-        printf("re[%d] = %f, rm[%d] = %f\n", N - 1, re[N - 1], N - 1, im[N - 1]);
+        // printf("re[%d] = %f, im[%d] = %f\n", N - 1, re[N - 1], N - 1, im[N - 1]);
         for (k = 0; k < N; k++)
         {
                 fprintf(fp2, "%d, %f, %f\n", k, re[k], im[k]);
@@ -178,44 +301,58 @@ int file_load_and_write(char filename1[], char filename2[],char function[]){
 
 int main(void)
 {
-        // 1次元離散フーリエ変換
         printf("ready...\n");
+        // 1次元離散フーリエ変換
         // file_load_and_write("test.csv", "inv_fourier.csv","dft");
         // printf("1dim dft is completed...\n");
+
+        // 1次元高速離散フーリエ変換
+        file_load_and_write("test.csv", "inv_fourier.csv","fft");
+        printf("1dim fft is completed...\n");
+
+        // 1次元逆離散フーリエ変換
         // file_load_and_write("inv_fourier.csv", "check.csv","idft");
         // printf("1dim idft is completed...\n");
+
+        // 1次元逆高速離散フーリエ変換
+        file_load_and_write("inv_fourier.csv", "check.csv","ifft");
+        printf("1dim ifft is completed...\n");
         // excelなどでグラフを書いて確認をしてみてください。
+
+
+
+
         // char path1[SIZE],blue_save_path[SIZE],gray_save_path[SIZE];
 
         // 2次元離散フーリエ変換
         //グレー画像
         //■配列の宣言
         Image<GRAY> gray, gout_dft, gout_idft;
+        // 画像のパスを各自の環境に変更をしてください。
         char path2[] = "C:\\Users\\ibuki\\program\\c\\ImageIO\\pictures\\lenna.pgm";
-        //■画像ロード
+        // // //■画像ロード
         gray.load(path2); //lenna.pgmをgrayにロードする
-        //■出力配列確保
+        // // //■出力配列確保
         gout_dft.create(gray.W, gray.H); // goutに img.W(画像imgの横幅) x img.H(画像imgの縦幅) の大きさの画素配列を用意する
         gout_idft.create(gray.W, gray.H);
         //■画像処理//////////////////////////////////////////////////////////////
         // Binarization(gray, gout, 128); //grayを閾値128で二値化してgoutに出力
-        // for(int i=0;i<gray.H;i++){
-        //         for(int j=0;j<gray.W;j++){
-        //                 printf("")
-        //         }
-        // }
-        // for(int i=0;i<gray.H;i++){
-        //         printf("gray.data[0][%d] = %d\n",i,gray.data[0][i]);
-        // }
 
-        two_dimension_fourier(gray,gout_dft);
+        // 1次元離散フーリエ変換
+        // two_dimension_fourier(gray,gout_dft,"dft");
+
+        // 1次元高速離散フーリエ変換
+        two_dimension_fourier(gray,gout_dft,"idft");
         // for(int i=0;i<gout_dft.H;i++){
         //         printf("gout_dft.data[0][%d] = %d\n",i,gout_dft.data[0][i]);
         // }
         char gout_dft_path[] = "C:\\Users\\ibuki\\program\\c\\ImageIO\\results\\two_dft_lenna.pgm";
         gout_dft.save(gout_dft_path); 
+        // 1次元逆離散フーリエ変換
+        // inverse_two_dimension_fourier(gout_dft,gout_idft,"idft");
 
-        inverse_two_dimension_fourier(gout_dft,gout_idft);
+        // 1次元逆高速離散フーリエ変換
+        inverse_two_dimension_fourier(gout_dft,gout_idft,"ifft");
 
         // for(int i=0;i<gout_idft.H;i++){
         //         for(int j=0;j<gout_idft.W;j++){
